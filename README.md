2b45ddbe036e2e3dff816fc54e7ac1e73fe22774
Junya Uchida <jyunya.uchida@gmail.com>
2021-07-12 00:01:41 +0900

    technique 追加

--- README.md
+++ README.md
@@ -8,4 +8,8 @@ $ python tarin_simsiam.py --data-dir C:/Users/Junya/Desktop/dataset_atmaCup11/ph
 $ python train_material.py --data-dir C:/Users/Junya/Desktop/dataset_atmaCup11 --batch-size 128
 ```
 
+* Materials
 class: 25 -> 5 にする
+
+* Techniques
+class: 10 -> 2 にする
--- datasets/atma_dataset.py
+++ datasets/atma_dataset.py
@@ -28,3 +28,27 @@ class MaterialAtmaDataset(data.Dataset):
         tar = torch.Tensor(list(tar))
 
         return img, tar
+
+class TechniqueAtmaDataset(data.Dataset):
+    """画像とtargetを返すdataset
+    """
+    def __init__(self, data_dir, img_name_df, target_df, trans):
+        self.data_dir = data_dir
+
+        self.img_name = list(img_name_df)
+        self.label = list(target_df.values)
+
+        self.trans = trans
+
+    def __len__(self):
+        return len(self.img_name)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.data_dir, self.img_name[idx] + '.jpg')
+        img = Image.open(img_path)
+        img = self.trans(img)
+
+        tar = self.label[idx]
+        tar = torch.Tensor(list(tar))
+
+        return img, tar
